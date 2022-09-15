1b88dde06fe48fda712cab1f2fae7f8a68287e1e
Keandrrre <k.kuzniar@share.epsb.ca>
2022-09-15 13:26:32 -0600

    HTML Done

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // Triangle Perimeter Assignment Start Code
+function btnClicked {
+    //Determine Vertex Values
+    let xA = +document.getElementById("xA").value;
+    let yA = +document.getElementById("yA").value;
+    let xB = +document.getElementById("xB").value;
+    let yB = +document.getElementById("yB").value;
+    let xC = +document.getElementById("xC").value;
+    let yC = +document.getElementById("yC").value;
+
+    //Calculate For AB
+    //Calculate For AC
+    //Calculate For BC
+}
